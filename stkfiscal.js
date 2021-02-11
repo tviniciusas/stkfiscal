cf05aad41bf74edbe1fe6e6cca05e9b52a806026
rafaeldev <rafac_santana@hotmail.com>
2021-02-11 16:46:28 -0300

    instalacao adminlte

--- stkfiscal.js
+++ stkfiscal.js
@@ -1,2 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const handlebars = require('express-handlebars');
+const app=express();
+
+app.engine("handlebars",handlebars({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+app.use('/css', express.static('css'))
+app.use('/js', express.static('js'))
+app.use('/img', express.static('img'))
+app.use('/plugins', express.static('plugins'))
+
+app.get("/:id?", function(req, res) {
+    //res.send("essa é a minha página inicial");
+    //res.sendFile(__dirname+"/index.html")
+    res.render('index', {id: req.params.id});
+})
+
+app.listen(3000, function(req, res) {
+    console.log('Servidor está rodando')
+})
 
