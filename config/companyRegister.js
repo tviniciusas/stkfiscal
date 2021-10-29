2ffae0e758181617d566f60cb759b7d2e911d026
andre10dias <andre10dias@ymail.com>
2021-10-28 22:04:59 -0300

    Desconsiderar

---
+++ config/companyRegister.js
@@ -0,0 +1,54 @@
+const puppeteer = require('puppeteer')
+const Empresa = require('../models/Empresa.js')
+const User = require('../models/User.js')
+
+
+module.exports = {
+
+    async registerCompany(req, res, next) {
+    
+        const companyCnpj = req.body.cadpcnpj
+    
+        const browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 100
+        })
+        const page = await browser.newPage()
+        await page.goto('http://www.sefaz.ba.gov.br/Sintegra/sintegra.asp?estado=BA')
+        await page.$eval('input[name=txt_CNPJ]', (el, value) => el.value = value, companyCnpj)
+        await page.click('input[name=Submit]')
+    
+        const nome = await page.evaluate(() => document.querySelector("body > form > table:nth-child(3) > tbody > tr > td:nth-child(2) > font ").innerHTML.slice(0, 100).replace('amp;','').replace(/\s{2,}/g, ' ').trim().trim().trim().trim().trim().trim().trim().trim().trim().trim().trim().trim().trim().trim().trim().trim())
+        const ie = await page.evaluate(() => document.querySelector("body > form > table:nth-child(2) > tbody > tr > td:nth-child(4) > font ").innerHTML.replace('&nbsp', '').replace(';', '').replace('.', '').replace('.', '').replace('.', '').trim().trim().trim().trim())
+        const uf = await page.evaluate(() => document.querySelector("body > form > table:nth-child(2) > tbody > tr > td:nth-child(6) > font").innerHTML)
+        const municipio = await page.evaluate(() => document.querySelector("body > form > table:nth-child(8) > tbody > tr > td:nth-child(4) > font").innerHTML.substring(0, 25).trim().trim().trim().trim().replace(/\s{2,}/g, ' '))
+        const email = await page.evaluate(() => document.querySelector("body > form > table:nth-child(9) > tbody > tr > td:nth-child(2) > font").innerHTML.substring(0, 40).trimEnd().trim().trim().trim().trim().trim().trim().trim())
+        const telefone = await page.evaluate(() => document.querySelector("body > form > table:nth-child(9) > tbody > tr > td:nth-child(4) > font").innerHTML.replace('&nbsp;', '').replace('(', '').replace(')', '').trimEnd().trim().trim().replace(/\s{2,}/g, ''))
+
+       await browser.close()
+        const users = await User.findOne({where: {empresas_id: req.body.empresas_id}});
+        const empresa_id = users.empresa_id
+      
+        const dadosCapturados = []
+
+        dadosCapturados.push(nome, companyCnpj, ie, telefone, email, uf, municipio)
+
+        console.log(dadosCapturados)
+        
+        const empresa = await Empresa.update({
+                    razao: nome,
+                    cnpj: companyCnpj,
+                    ie,
+                    telefone,
+                    email,
+                    estado: uf,
+                    cidade: municipio
+        }).then(() => {
+            req.flash("success_msg", 'Empresa cadastrada')
+            next()
+            res.redirect('/empresa')
+        }).catch(e => {
+            req.flash("error_msg", 'Erro ao cadastrar empresa ' + e)
+        })
+    j}
+}
---
+++ config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  host:"localhost",
+  dialect:"mysql",
+  username: "root",
+  password: "1234",
+  database: "istock",
+  timezone: "-03:00",
+  define: {
+      timestamps: true,
+      underscored: true,
+  }
+}
