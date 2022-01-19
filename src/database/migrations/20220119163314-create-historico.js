aa1980c85a3464bfbb3f09dbfa632f2ed4e1ab12
andre10dias <andre10dias@ymail.com>
2022-01-19 13:50:27 -0300

    Migration histórico

---
+++ src/database/migrations/20220119163314-create-historico.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('historicos', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      data_hora: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      empresa: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      descricao: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      solicitacao_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {model: 'solicitacoes', key: 'id'}
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      solicitaco_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('historicos');
+  }
+};
